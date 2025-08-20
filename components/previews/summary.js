55038a6e9e30dcc2cce12e25ba89faeb83f1f603
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2025-08-20 19:06:58 +0200

    changed html render

--- components/previews/summary.js
+++ components/previews/summary.js
@@ -1,14 +1,17 @@
-import renderHTML from "react-render-html";
+import parse from "html-react-parser";
 
 export default function Summary({ resume }) {
   return (
     <div className="mt-2">
-      <h2 className="font-bold mb-2" style={{ color: resume?.themeColor }}>Professional Summary</h2>
+      <h2 className="font-bold mb-2" style={{ color: resume?.themeColor }}>
+        Professional Summary
+      </h2>
       {resume.summary && (
         <div className="text-xs font-normal">
-          {renderHTML(resume.summary)}
+          {parse(resume.summary)}
         </div>
       )}
     </div>
   );
 }
+
