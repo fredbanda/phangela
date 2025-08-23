079c660ee444f6d6e852557005a80fc2ccc161ce
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2025-08-23 04:19:32 +0200

    landing page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import HeroBanner from "@/components/homepage/hero";
+import HowItWorks from "@/components/homepage/steps";
 
 export default function Home() {
 
   return (
-    <div className="flex flex-col items-center justify-center max-h-screen py-2 mt-[180px] ">
-      <h2>Free CV Resume</h2>
-      <Link href="/resume/create">
-      <Button variant="default">Start Creating </Button>
-      </Link>
+    <div className="w-full flex flex-col items-center justify-center py-2 ">
+    <HeroBanner />
+      <HowItWorks />
     </div>
   );
 }
